e0f0375b04d77253ea057ef7b954ee6eeb299e90
Samantha Thong <samantha.thong@epita.fr>
2015-06-05 20:23:57 +0200

    Structure refactor

---
+++ app/config/services.php
@@ -0,0 +1,44 @@
+<?php
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Session\Adapter\Files as Session;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+$di->set('db', function() use ($config) {
+    return new DbAdapter(array(
+        'host'     => $config->database->host,
+        'username' => $config->database->username,
+        'password' => $config->database->password,
+        'dbname'   => $config->database->name
+    ));
+});
+
+$di->set('url', function() use ($config) {
+    $url = new UrlProvider();
+    $url->setBaseUri($config->application->baseUri);
+    return $url;
+});
+
+// Views
+$di->set('view', function() use ($config) {
+    $view = new View();
+    $view->setViewsDir(APP_PATH . $config->application->viewsDir);
+    return $view;
+});
+
+// Session
+$di->set('session', function() {
+    $session = new Session();
+    $session->start();
+    return $session;
+});
+
+// Database
+$di->set('db', function() use ($config) {
+    return new DbAdapter(array(
+        'host'     => $config->database->host,
+        'username' => $config->database->username,
+        'password' => $config->database->password,
+        'dbname'   => $config->database->name
+    ));
+});
